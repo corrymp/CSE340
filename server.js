4435a15863a8628d1502107b31ca8cd4a736d6c9
corrymp <corrympalmer@gmail.com>
2025-05-10 15:31:29 -0700

    W01 | Add home page | Add 404 page | Add styles

--- server.js
+++ server.js
@@ -3,24 +3,21 @@ const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 const env = require('dotenv').config();
 const app = express();
-const static = require('./routes/static');
+const staticContent = require('./routes/static');
 
-/* Routes */
+/* Layout */
 app.set('view engine', 'ejs');
 app.use(expressLayouts);
 app.set('layout', './layouts/layout');
 
-/* Routes */
-app.use(static);
-app.get('/', (req, res) => {
-    res.render('index', {title:'Home'});
-});
+/* Routes: {static}, [/, /home], 404 fallback */
+app.use(staticContent);
+app.get(['/', '/home'], (req, res) => res.render('index', {title: 'Home'}));
+app.get(/\/.+/, (req, res) => res.render('notfound', {title: 'Page Not Found', path: req._parsedUrl.href}));
 
 /* Local Server Information Values from .env (environment) file */
 const port = process.env.PORT;
 const host = process.env.HOST;
 
 /* Log statement to confirm server operation */
-app.listen(port, () => {
-  console.log(`app listening on ${host}:${port}`);
-});
+app.listen(port, () => console.log(`app listening on ${host}:${port}`));
